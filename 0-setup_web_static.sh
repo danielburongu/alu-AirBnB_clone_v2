4ea8198e2d211d0efe3042ad2feb4b627cfeecac
danielburongu <d.burongu@alustudent.com>
2023-06-21 10:23:36 +0200

    0-setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,47 +1,48 @@
 #!/usr/bin/env bash
+# sets up my web servers for the deployment of web_static
 
 echo -e "\e[1;32m START\e[0m"
 
-# Updating the packages
+#--Updating the packages
 sudo apt-get -y update
 sudo apt-get -y install nginx
 echo -e "\e[1;32m Packages updated\e[0m"
 echo
 
-# Configure firewall
+#--configure firewall
 sudo ufw allow 'Nginx HTTP'
-echo -e "\e[1;32m Allow incoming NGINX HTTP connections\e[0m"
+echo -e "\e[1;32m Allow incomming NGINX HTTP connections\e[0m"
 echo
 
-# Create the directories
+#--created the dir
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo -e "\e[1;32m Directories created\e[0m"
+echo -e "\e[1;32m directories created"
 echo
 
-# Add test string
-echo "<h1>Welcome to www.beta-scribbles.tech</h1>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+#--adds test string
+echo "<h1>Welcome to www.beta-scribbles.tech</h1>" > /data/web_static/releases/test/index.html
 echo -e "\e[1;32m Test string added\e[0m"
 echo
 
-# Prevent overwrite
-if [ -d "/data/web_static/current" ]; then
-    echo "Path /data/web_static/current exists"
-    sudo rm -rf /data/web_static/current
-fi
-echo -e "\e[1;32m Prevent overwrite\e[0m"
+#--prevent overwrite
+if [ -d "/data/web_static/current" ];
+then
+    echo "path /data/web_static/current exists"
+    sudo rm -rf /data/web_static/current;
+fi;
+echo -e "\e[1;32m prevent overwrite\e[0m"
 echo
 
-# Create symbolic link
+#--create symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data
+
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
 echo -e "\e[1;32m Symbolic link created\e[0m"
 echo
 
-# Update Nginx configuration
-sudo sed -i '/^server {/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
-echo -e "\e[1;32m Nginx configuration updated\e[0m"
-echo
-
-# Restart Nginx
+#--restart NGINX
 sudo service nginx restart
-echo -e "\e[1;32m Nginx restarted\e[0m"
+echo -e "\e[1;32m restart NGINX\e[0m"
